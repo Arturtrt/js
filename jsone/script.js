8e5a0a5791729c21c7caecb2298af37a1cfbae56
Arturtrt <anevelsky@mail.ru>
2022-08-10 20:32:11 +0300

    abbb

---
+++ jsone/script.js
@@ -0,0 +1,16 @@
+'use strict'
+//number  1, 2.. +(-)infinity 0.2 NaN
+//string "sad" 'dog' `cow`
+//boolean true false
+//object {}, [], null
+//undefined undefined
+//null =0
+//bigInt 1n
+//symbol {}
+// +, -, /, *, **, %, =, ==, ===
+// var, let, const
+const numFirst = 1;
+numFerst = "qwer";
+let num = 2;
+var dog = "bin";
+console.log(typeof null);
